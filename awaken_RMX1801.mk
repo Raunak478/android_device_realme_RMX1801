6155d710c7353f39c7d7bd3564b8974d9d2ce227
Raunak478 <122750258+Raunak478@users.noreply.github.com>
2023-07-08 18:53:10 +0530

    Fixed

--- awaken_RMX1801.mk
+++ awaken_RMX1801.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Awaken stuff
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
 # Inherit from Realme RMX1801
